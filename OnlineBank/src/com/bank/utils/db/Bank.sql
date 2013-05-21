c401964be4b58f32a50631e010cd9785bc4c0645
zarvis.a <zarvis.a@gmail.com>
2013-05-21 10:29:12 +0000

    Changed the TableName

--- OnlineBank/src/com/bank/utils/db/Bank.sql
+++ OnlineBank/src/com/bank/utils/db/Bank.sql
@@ -2,4 +2,4 @@ create database bankdb;
 
 use bankdb;
 
-create table AppUser(userName varchar(255) NOT NULL PRIMARY KEY, password varchar(255), userRole varchar(255));
+create table BankUser(userName varchar(255) NOT NULL PRIMARY KEY, password varchar(255), userRole varchar(255));
