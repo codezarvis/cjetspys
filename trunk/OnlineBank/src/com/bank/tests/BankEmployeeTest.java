96e74d70f8a26169480f7cc52f70915f82071d61
priyanka.akula27@gmail.com <priyanka.akula27@gmail.com@e6418d45-5790-35c7-7212-7777224da489>
2013-05-21 09:51:22 +0000

    modified suceefuly...

--- trunk/OnlineBank/src/com/bank/tests/BankEmployeeTest.java
+++ trunk/OnlineBank/src/com/bank/tests/BankEmployeeTest.java
@@ -1,5 +1,6 @@
 package com.bank.tests;
 
+import com.bank.app.domain.BankUser;
 import com.bank.domain.BankEmployee;
 
 public class BankEmployeeTest {
@@ -9,12 +10,12 @@ public class BankEmployeeTest {
 	 */
 	public static void main(String[] args) {
 		 
-		BankEmployee bankEmployee = new BankEmployee();
-		bankEmployee.setEmployeeId(101);
-		bankEmployee.setEmployeeName("priyanka");
-		bankEmployee.setSalary(300);
-		System.out.println(bankEmployee.getEmployeeId()+"\t"+bankEmployee.getEmployeeName()+"\t"
-		                  +bankEmployee.getSalary()+"\n");
+		BankUser bankUser = new BankEmployee();
+		bankUser.setEmployeeId(101);
+		bankUser.setEmployeeName("priyanka");
+		bankUser.setSalary(300);
+		System.out.println(bankUser.getEmployeeId()+"\t"+bankUser.getEmployeeName()+"\t"
+		                  +bankUser.getSalary()+"\n");
 		
 
 	}
