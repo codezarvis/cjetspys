4aad50e0c2f8adaf7bce8a6103b9b1ed52160300
zarvis.a <zarvis.a@gmail.com>
2013-05-21 09:23:05 +0000

    Commited utils for the App

---
+++ OnlineBank/src/com/bank/utils/queries/BankEmployeeQueries.java
@@ -0,0 +1,14 @@
+package com.bank.utils.queries;
+
+/**
+ * @author Sudarsan
+ *
+ */
+public interface BankEmployeeQueries {
+
+	
+	public static final String INSERT = "";
+	public static final String UPDATE = "";
+	public static final String DELETE = "";
+	public static final String AUTHENTICATE = "";
+}
