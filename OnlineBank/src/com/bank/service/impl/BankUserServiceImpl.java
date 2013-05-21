6e3d827b58e96b6824239ecfe714c7a981592931
gyaswanthi.gyaswanthi <gyaswanthi.gyaswanthi@gmail.com>
2013-05-21 10:12:39 +0000

    Added BankUserServiceimpl

--- OnlineBank/src/com/bank/service/impl/BankUserServiceImpl.java
+++ OnlineBank/src/com/bank/service/impl/BankUserServiceImpl.java
@@ -1,5 +1,21 @@
 package com.bank.service.impl;
 
-public class BankUserServiceImpl {
+import java.util.List;
+
+import com.bank.app.domain.BankUser;
+
+public class BankUserServiceImpl implements BankUserService{
+
+	
+	public void create(BankUser bankUser) {
+		//To do
+		
+	}
+
+	
+	public List<BankUser> getAll() {
+		//to do
+		return null;
+	}
 
 }
