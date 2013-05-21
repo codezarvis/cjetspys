56bf67ce556ddca0ef9571a04dfb805a5ae4f777
zarvis.a@gmail.com <zarvis.a@gmail.com@e6418d45-5790-35c7-7212-7777224da489>
2013-05-21 09:43:49 +0000

    Changed domain package

--- trunk/OnlineBank/src/com/bank/domain/BankEmployee.java
+++ trunk/OnlineBank/src/com/bank/domain/BankEmployee.java
@@ -1,30 +1,81 @@
 package com.bank.domain;
 
-public class BankEmployee {
+import com.bank.app.domain.BankUser;
+
+/**
+ * @author Sudarsan
+ * 
+ */
+public class BankEmployee implements BankUser {
 
 	private int employeeId;
 	private String employeeName;
 	private double salary;
+
+	/* (non-Javadoc)
+	 * @see com.bank.domain.BankUser#getEmployeeId()
+	 */
+	@Override
 	public int getEmployeeId() {
 		return employeeId;
 	}
+
+	/* (non-Javadoc)
+	 * @see com.bank.domain.BankUser#setEmployeeId(int)
+	 */
+	@Override
 	public void setEmployeeId(int employeeId) {
 		this.employeeId = employeeId;
 	}
+
+	/* (non-Javadoc)
+	 * @see com.bank.domain.BankUser#getEmployeeName()
+	 */
+	@Override
 	public String getEmployeeName() {
 		return employeeName;
 	}
+
+	/* (non-Javadoc)
+	 * @see com.bank.domain.BankUser#setEmployeeName(java.lang.String)
+	 */
+	@Override
 	public void setEmployeeName(String employeeName) {
 		this.employeeName = employeeName;
 	}
+
+	/* (non-Javadoc)
+	 * @see com.bank.domain.BankUser#getSalary()
+	 */
+	@Override
 	public double getSalary() {
 		return salary;
 	}
+
+	/* (non-Javadoc)
+	 * @see com.bank.domain.BankUser#setSalary(double)
+	 */
+	@Override
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
-	
-	
-	
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("BankEmployee [employeeId=");
+		builder.append(employeeId);
+		builder.append(", employeeName=");
+		builder.append(employeeName);
+		builder.append(", salary=");
+		builder.append(salary);
+		builder.append("]");
+		return builder.toString();
+	}
+
 }
