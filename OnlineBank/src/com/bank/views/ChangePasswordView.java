eb130b1c0036e3f8050b1cd80ff0fd8f1bab05fe
priyanka.akula27 <priyanka.akula27@gmail.com>
2013-05-22 12:00:51 +0000

    done

--- OnlineBank/src/com/bank/views/ChangePasswordView.java
+++ OnlineBank/src/com/bank/views/ChangePasswordView.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JPasswordField;
 
+@SuppressWarnings("serial")
 public class ChangePasswordView extends JFrame{
 	private JTextField textField;
 	private JPasswordField passwordField;
